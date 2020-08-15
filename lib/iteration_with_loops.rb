cfcac12f531e2a01f74174682ed4c5249ec066d7
Bryam Vicente <vicentebryam@gmail.com>
2020-08-15 16:07:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,36 +2,33 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 
-  # count = 0
-  #   solution_answer = []
-  #   sorted_array = []
-  #   src.each do |little_array|
-  #     sorted_array << little_array.sort
-  #   end
-  words = []
-  sorted_array = []
-  count = 0
-  src.each do |items|
-    items.each do |item|
-      if item.is_a?(String)
-        words << item
-      end
-    end
-  end
+  def find_min_in_nested_arrays(src)
 
-    while count < sorted_array.count do
-    inner_count = 0
-    minimum_amount = ""
-    while inner_count < sorted_array.count do
-      if sorted_array[count][inner_count] < minimum_amount
-        minimum_amount = sorted_array[count][inner_count]
-        # inner_count += 1
-      end
-      inner_count += 1
-      # count += 1
+words = []
+sorted_array = []
+count = 0
+src.each do |items|
+  items.each do |item|
+    if item.is_a?(String)
+      words << item
     end
-    words << minimum_amount
-    count += 1
-    end
-  words
   end
+end
+
+
+  while count < sorted_array.count do
+  inner_count = 0
+  minimum_amount = ""
+  while inner_count < sorted_array.count do
+    if sorted_array[count][inner_count] < minimum_amount
+      minimum_amount = sorted_array[count][inner_count]
+      # inner_count += 1
+    end
+    inner_count += 1
+    # count += 1
+  end
+  words << minimum_amount
+  count += 1
+  end
+  words
+end
