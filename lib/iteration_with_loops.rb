3334835f26a45a5e3a50bc6c3914b96d72694aa8
Bryam Vicente <vicentebryam@gmail.com>
2020-08-15 15:47:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,22 +1,37 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  total = 0
-  row_index = 0
-  sentence = src do |item|
-    items.select { |item| item.is_a?(String) }
-  end.join(" ")
   
-
-
-  while row_index < src.count do
-    element_index = 0
-    strings = ""
-    while element_index < src[row_index].count do
-      total += src[row_index][element_index]
-      element_index += 1
-  end
-  row_index += 1
+  # count = 0
+  #   solution_answer = []
+  #   sorted_array = []
+  #   src.each do |little_array|
+  #     sorted_array << little_array.sort
+  #   end
+  words = []
+  sorted_array = []
+  count = 0
+  src.each do |items|
+    items.each do |item|
+      if item.is_a?(String)
+        words << item
+      end
     end
-    total
-end
+  end
+
+    while count < sorted_array.count do
+    inner_count = 0
+    minimum_amount = ""
+    while inner_count < sorted_array.count do
+      if sorted_array[count][inner_count] < minimum_amount
+        minimum_amount = sorted_array[count][inner_count]
+        # inner_count += 1
+      end
+      inner_count += 1
+      # count += 1
+    end
+    solution_answer << minimum_amount
+    count += 1
+    end
+  words
+  end
